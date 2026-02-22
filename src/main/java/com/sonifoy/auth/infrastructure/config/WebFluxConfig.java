cbd6448c6fadc3e21848e3701b238788e3c5e24b
Harvey Asprilla <kikiyam_8@hotmail.com>
2026-02-21 20:02:43 -0500

    fix: explicit webflux cors and trailing slash config

---
+++ src/main/java/com/sonifoy/auth/infrastructure/config/WebFluxConfig.java
@@ -0,0 +1,16 @@
+package com.sonifoy.auth.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class WebFluxConfig implements WebFluxConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
